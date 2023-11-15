c14eb3e480e944f605937d7decc56c1e9af5b034
driesdumolein <44809403+driesdumolein@users.noreply.github.com>
2023-11-15 14:48:43 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,10 +9,10 @@ output "result" {
       password = ""
     }
     // UCP resource IDs
-    resources = [
-        "/planes/kubernetes/local/namespaces/${kubernetes_service.metadata.namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
-        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
-    ]
+    // resources = [
+    //     "/planes/kubernetes/local/namespaces/${kubernetes_service.metadata.namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
+    //     "/planes/kubernetes/local/namespaces/${kubernetes_deployment.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
+    // ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
   sensitive = true
